7be0b675c3b7d6b04f3c80ccdfd6209af9802beb
KwameSei <nathanielosei92@gmail.com>
2023-07-20 11:14:35 +0000

    Frontend Basic Setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 import app from './app.js';
+import mongoose from 'mongoose';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5010;
+const url = process.env.DB_URL;
 
-app.listen(port, () => {
+// Connect to MongoDB
+mongoose.connect(url, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+  });
+}).catch(error => {
+  console.error('Error connecting to MongoDB', error.message);
 });
 
 export default app;
