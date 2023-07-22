79bcd09af78e04c8c0d55836387260d80dc68b4f
KwameSei <nathanielosei92@gmail.com>
2023-07-22 11:22:07 +0000

    Header creation started

--- frontend/src/widgets/header/Header.jsx
+++ frontend/src/widgets/header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+// import { images } from '../../assets/images'
 
 import './Header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header header__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='header__info'
+      >
+        <h1 className='header__title'>Hi, I'm <span className='header__title--name'>Sergio</span></h1>
+      </motion.div>
+    </div>
   )
 }
 
